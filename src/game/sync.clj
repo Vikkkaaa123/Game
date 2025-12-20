f74ad5c57f17cfede559a3a6b68da54f10b3eee9
Vikkkaaa123 <pereuznikviktoria@gmail.com>
2025-12-20 22:32:00 +0300

    Create sync.clj

---
+++ src/game/sync.clj
@@ -0,0 +1,8 @@
+(ns game.sync
+  "Простая синхронизация")
+
+(defn sync-all-rooms []
+  (println "[SYNC] Комнаты синхронизированы (заглушка)"))
+
+(defn sync-all-players []
+  (println "[SYNC] Игроки синхронизированы (заглушка)"))
