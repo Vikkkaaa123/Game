9723aed72c4fe20792c18cd66d546a939aeacb18
Vikkkaaa123 <pereuznikviktoria@gmail.com>
2025-12-21 21:01:53 +0300

    Update core.clj

--- src/game/core.clj
+++ src/game/core.clj
@@ -12,113 +12,142 @@
 (defn handle-command [player-name input]
   (try
     (let [parts (str/split input #" ")
-          command (str/lower-case (first parts))
+          ;; ИСПРАВЛЕНИЕ 1: обрезаем пробелы и приводим к нижнему регистру
+          command (str/trim (str/lower-case (first parts)))
           args (str/join " " (rest parts))]
       
-      (case command
+      (cond
+        ;; ИСПРАВЛЕНИЕ 2: все варианты команд через cond вместо case
         ;; Осмотреться
-        "look" (let [room (world/get-player-room player-name)]
-                 (str "Вы в " (world/get-room-name room) "\n"
-                      (world/get-room-desc room) "\n"
-                      "Предметы: " (if-let [items (world/get-room-items room)]
-                                     (if (empty? items)
-                                       "нет"
-                                       (str/join ", " (map items/get-item-name items)))
-                                     "нет") "\n"
-                      "Игроки: " (if-let [players (world/get-room-players room)]
-                                   (if (empty? (disj players player-name))
-                                     "никого"
-                                     (str/join ", " (disj players player-name)))
-                                   "никого")))
+        (contains? #{"look" "осмотреть" "посмотреть" "осмотреться" "l"} command)
+        (let [room (world/get-player-room player-name)]
+          (str "Вы в " (world/get-room-name room) "\n"
+               (world/get-room-desc room) "\n"
+               "Предметы: " (if-let [room-items (world/get-room-items room)]
+                              (if (empty? room-items)
+                                "нет"
+                                ;; ИСПРАВЛЕНИЕ 3: правильное получение имен предметов
+                                (let [item-names (keep items/get-item-name room-items)]
+                                  (if (empty? item-names)
+                                    "нет"
+                                    (str/join ", " item-names))))
+                              "нет") "\n"
+               "Игроки: " (if-let [players (world/get-room-players room)]
+                            (let [other-players (disj players player-name)]
+                              (if (empty? other-players)
+                                "никого"
+                                (str/join ", " other-players)))
+                            "никого")))
         
         ;; Взять предмет
-        "взять" (if (empty? args)
-                  "Укажите предмет: взять [предмет]"
-                  (let [room (world/get-player-room player-name)
-                        room-items (world/get-room-items room)
-                        ;; Ищем предмет по частичному совпадению имени
-                        item-key (first (filter #(str/includes? 
-                                                  (str/lower-case (items/get-item-name %)) 
-                                                  (str/lower-case args))
-                                                room-items))]
-                    (if item-key
-                      (do
-                        (world/add-to-inventory! player-name item-key)
-                        (world/remove-item-from-room! room item-key)
-                        (str "Вы взяли: " (items/get-item-name item-key)))
-                      "Такого предмета здесь нет")))
+        (contains? #{"взять" "take" "поднять" "t"} command)
+        (if (str/blank? args)
+          "Укажите предмет: взять [предмет]"
+          (let [room (world/get-player-room player-name)
+                room-items (world/get-room-items room)
+                ;; Ищем предмет по частичному совпадению имени
+                item-key (first (filter #(let [item-name (items/get-item-name %)]
+                                           (and item-name 
+                                                (str/includes? 
+                                                 (str/lower-case item-name) 
+                                                 (str/lower-case args))))
+                                        room-items))]
+            (if item-key
+              (do
+                (world/add-to-inventory! player-name item-key)
+                (world/remove-item-from-room! room item-key)
+                (str "Вы взяли: " (items/get-item-name item-key)))
+              "Такого предмета здесь нет")))
         
         ;; Положить предмет
-        "положить" (if (empty? args)
-                     "Укажите предмет: положить [предмет]"
-                     (let [inventory (world/get-player-inventory player-name)
-                           ;; Ищем предмет в инвентаре
-                           item-key (first (filter #(str/includes?
-                                                     (str/lower-case (items/get-item-name %)) 
-                                                     (str/lower-case args))
-                                                   inventory))]
-                       (if item-key
-                         (do
-                           (world/remove-from-inventory! player-name item-key)
-                           (let [room (world/get-player-room player-name)]
-                             (world/add-item-to-room! room item-key))
-                           (str "Вы положили: " (items/get-item-name item-key)))
-                         "У вас нет такого предмета")))
+        (contains? #{"положить" "drop" "бросить" "пол"} command)
+        (if (str/blank? args)
+          "Укажите предмет: положить [предмет]"
+          (let [inventory (world/get-player-inventory player-name)
+                ;; Ищем предмет в инвентаре
+                item-key (first (filter #(let [item-name (items/get-item-name %)]
+                                           (and item-name
+                                                (str/includes?
+                                                 (str/lower-case item-name)
+                                                 (str/lower-case args))))
+                                        inventory))]
+            (if item-key
+              (do
+                (world/remove-from-inventory! player-name item-key)
+                (let [room (world/get-player-room player-name)]
+                  (world/add-item-to-room! room item-key))
+                (str "Вы положили: " (items/get-item-name item-key)))
+              "У вас нет такого предмета")))
         
         ;; Показать инвентарь
-        "инвентарь" (let [inv (world/get-player-inventory player-name)]
-                      (if (empty? inv)
-                        "Ваш инвентарь пуст"
-                        (str "Ваш инвентарь:\n" 
-                             (str/join "\n" (map items/get-item-name inv)))))
+        (contains? #{"инвентарь" "inventory" "инв" "i"} command)
+        (let [inventory (world/get-player-inventory player-name)]
+          (if (empty? inventory)
+            "Ваш инвентарь пуст"
+            (str "Ваш инвентарь:\n" 
+                 (str/join "\n" (keep items/get-item-name inventory)))))
         
         ;; Перемещение
-        "идти" (if (empty? args)
-                 "Укажите направление: идти [север/юг/запад/восток]"
-                 (let [direction (keyword args)
-                       current-room (world/get-player-room player-name)
-                       target-room (world/get-exit-room current-room direction)]
-                   (if target-room
-                     (do
-                       (world/set-player-room! player-name target-room)
-                       (handle-command player-name "look"))
-                     "Нельзя пойти в этом направлении")))
+        (contains? #{"идти" "go" "g"} command)
+        (if (str/blank? args)
+          "Укажите направление: идти [север/юг/запад/восток]"
+          (let [direction-map {"север" :north "юг" :south "запад" :west "восток" :east
+                               "north" :north "south" :south "west" :west "east" :east}
+                direction (get direction-map (str/lower-case args))
+                current-room (world/get-player-room player-name)
+                target-room (when direction (world/get-exit-room current-room direction))]
+            (if target-room
+              (do
+                (world/set-player-room! player-name target-room)
+                (handle-command player-name "look"))
+              "Нельзя пойти в этом направлении")))
         
         ;; Короткие команды движения
-        "с" (handle-command player-name "идти север")
-        "ю" (handle-command player-name "идти юг")
-        "з" (handle-command player-name "идти запад")
-        "в" (handle-command player-name "идти восток")
+        (contains? #{"с" "север" "north" "n"} command) 
+        (handle-command player-name "идти север")
+        
+        (contains? #{"ю" "юг" "south" "s"} command)
+        (handle-command player-name "идти юг")
+        
+        (contains? #{"з" "запад" "west" "w"} command)
+        (handle-command player-name "идти запад")
+        
+        (contains? #{"в" "восток" "east" "e"} command)
+        (handle-command player-name "идти восток")
         
         ;; Сказать в комнате
-        "сказать" (if (empty? args)
-                    "Скажите что-нибудь: сказать [текст]"
-                    (let [room (world/get-player-room player-name)
-                          other-players (world/get-players-in-room room player-name)]
-                      (str "Вы сказали: \"" args "\"\n"
-                           (if (empty? other-players)
-                             "Но в комнате никого нет"
-                             (str "Вас слышат: " (str/join ", " other-players))))))
+        (contains? #{"сказать" "say" "с"} command)
+        (if (str/blank? args)
+          "Скажите что-нибудь: сказать [текст]"
+          (let [room (world/get-player-room player-name)
+                other-players (world/get-players-in-room room player-name)]
+            (str "Вы сказали: \"" args "\"\n"
+                 (if (empty? other-players)
+                   "Но в комнате никого нет"
+                   (str "Вас слышат: " (str/join ", " other-players))))))
         
         ;; Помощь
-        "помощь" (str "Доступные команды:\n"
-                      "look - осмотреться\n"
-                      "взять [предмет] - взять предмет\n"
-                      "положить [предмет] - положить предмет\n"
-                      "инвентарь - показать инвентарь\n"
-                      "идти [север/юг/запад/восток] - переместиться\n"
-                      "с/ю/з/в - короткие команды движения\n"
-                      "сказать [текст] - сказать в комнате\n"
-                      "помощь - эта справка\n"
-                      "выход - выйти из игры")
+        (contains? #{"помощь" "help" "справка" "h" "?"} command)
+        (str "Доступные команды:\n"
+             "look / осмотреть - осмотреться\n"
+             "взять [предмет] / take - взять предмет\n"
+             "положить [предмет] / drop - положить предмет\n"
+             "инвентарь / inventory - показать инвентарь\n"
+             "идти [север/юг/запад/восток] / go - переместиться\n"
+             "с/ю/з/в / n/s/w/e - короткие команды движения\n"
+             "сказать [текст] / say - сказать в комнате\n"
+             "помощь / help - эта справка\n"
+             "выход / exit - выйти из игры")
         
         ;; Выход из игры
-        "выход" (do
-                  (world/remove-player! player-name)
-                  (reset! game-running false)
-                  "До свидания!")
+        (contains? #{"выход" "exit" "quit" "выйти" "q"} command)
+        (do
+          (world/remove-player! player-name)
+          (reset! game-running false)
+          "До свидания!")
         
         ;; Неизвестная команда
+        :else
         "Неизвестная команда. Введите 'помощь' для списка команд."))
     
     (catch Exception e
@@ -128,7 +157,8 @@
 (defn -main
   "Точка входа в игру"
   [& args]
-  (println "ИГРА 'ПОБЕГ ИЗ ЛАБОРАТОРИИ'")
+  (println "🎮 ИГРА 'ПОБЕГ ИЗ ЛАБОРАТОРИИ'")
+  (println "==============================")
   (println "Кооперативная текстовая игра на Clojure с STM")
   (println "Создано командой разработчиков")
   
@@ -163,23 +193,37 @@
 
 ;; Для работы в REPL (без запуска сервера)
 (comment
-  ;; Ручное тестирование в REPL:
+  ;; Тестирование в REPL
+  (require '[game.world :as world])
+  (require '[game.items :as items])
   
   ;; 1. Добавить игрока
   (world/add-player! "Тест")
   
   ;; 2. Посмотреть комнату
   (handle-command "Тест" "look")
+  (handle-command "Тест" "осмотреть")
+  (handle-command "Тест" "l")
   
-  ;; 3. Взять предмет (если есть)
+  ;; 3. Взять предмет
   (handle-command "Тест" "взять ключ")
+  (handle-command "Тест" "take ключ")
+  (handle-command "Тест" "t ключ")
   
-  ;; 4. Посмотреть инвентарь
+  ;; 4. Инвентарь
   (handle-command "Тест" "инвентарь")
+  (handle-command "Тест" "inventory")
+  (handle-command "Тест" "i")
   
-  ;; 5. Переместиться
-  (handle-command "Тест" "идти север")
+  ;; 5. Помощь
+  (handle-command "Тест" "помощь")
+  (handle-command "Тест" "help")
+  (handle-command "Тест" "h")
+  (handle-command "Тест" "?")
   
-  ;; 6. Выйти
+  ;; 6. Выход
   (handle-command "Тест" "выход")
+  (handle-command "Тест" "exit")
+  (handle-command "Тест" "quit")
+  (handle-command "Тест" "q")
 )
