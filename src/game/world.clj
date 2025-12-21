c7da9f54910cfb56e70911c1d42b3356556652ae
Vikkkaaa123 <pereuznikviktoria@gmail.com>
2025-12-21 20:18:30 +0300

    Update world.clj

--- src/game/world.clj
+++ src/game/world.clj
@@ -1,467 +1,291 @@
-(ns game.game.world
+(ns game.world
   "Ядро игрового мира - STM система.
-   Главное состояние игры: rooms, players, items, puzzles.
-   Все изменения через транзакции dosync.")
+   Только состояние комнат и игроков. Предметы в отдельном модуле."
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]))
+
+
 
 ;; ГЛАВНОЕ СОСТОЯНИЕ МИРА
 
 (defonce world-state
   (ref {
-
-        
-    ;; КОМНАТЫ (основная структура мира)
-
-    :rooms {
-      :start {
-        :name "Стартовая комната"
-        :desc "Вы находитесь в заброшенной лаборатории. Вокруг разбросано оборудование. Стены исписаны формулами. На столе микроскоп и разобранный прибор."
-        :exits {:north :hallway}
-        :items #{"ключ-карта" "микроскоп" "провод"}
-        :puzzle nil
-        :players #{}
-        :locked-exits #{}
-      }
-      
-      :hallway {
-        :name "Коридор"
-        :desc "Длинный коридор с дверьми по обеим сторонам. На стене висит схема лаборатории. Одна дверь ведет в архив, другая - в серверную."
-        :exits {:south :start :east :promenade :west :archive}
-        :items #{"схема"}
-        :puzzle {
-          :type :door-code
-          :solved false
-          :hint "Код от серверной: 3XX7 (X = номер месяца основания лаборатории)"
-          :solution "3107"
-          :attempts 0
-        }
-        :players #{}
-        :locked-exits #{:west}  ;; Дверь в архив закрыта
-      }
-      
-      :promenade {
-        :name "Променад"
-        :desc "Просторное помещение с панорамным окном. Вид на закрытый двор лаборатории. В центре стоит неработающая консоль управления."
-        :exits {:west :hallway}
-        :items #{"консоль"}
-        :puzzle {
-          :type :repair-console
-          :solved false
-          :required-items #{"провод" "схема"}
-          :message "Консоль требует починки. Нужны провод и схема подключения."
-        }
-        :players #{}
-        :locked-exits #{}
-      }
-      
-      :archive {
-        :name "Архив"
-        :desc "Комната с архивными стеллажами. Пахнет старыми книгами и пылью. На столе лежат журналы и документы."
-        :exits {:east :hallway}
-        :items #{"лабораторный-журнал" "формулы"}
-        :puzzle {
-          :type :find-date
-          :solved false
-          :question "В каком месяце была основана лаборатория? (номер месяца)"
-          :answer "10"
-          :found false
-        }
-        :players #{}
-        :locked-exits #{}  ;; Изначально открыта, но доступ через hallway заблокирован
-      }
-    }
+    ;; КОМНАТЫ (загружаются из .edn)
+    :rooms {}
     
-
-    ;; ИГРОКИ (динамически заполняется при подключении)
-
-    :players {}  ;; Структура: {"имя" {:room :start :inventory #{} ...}}
+    ;; ИГРОКИ {имя {:room :комната :inventory #{}}}
+    :players {}
     
-
-    ;; ГОЛОВОЛОМКИ (глобальное состояние)
-
-        
-    :puzzles {
-      :main-door {:solved false :required [:lab-solved :console-solved]}
-      :lab-solved false
-      :console-solved false
-      :archive-accessed false
-    }
-
-
-        
-    ;; ПРЕДМЕТЫ (глобальный реестр)
-
-    :items-registry {
-      "ключ-карта" {
-        :name "Ключ-карта"
-        :desc "Пластиковая карта с магнитной полосой. Имеет логотип лаборатории."
-        :type :key
-        :usable-in [:hallway]
-        :effect "Открывает дверь в архив при правильном коде"
-      }
-      "микроскоп" {
-        :name "Микроскоп"
-        :desc "Электронный микроскоп. Под линзой слайд с цифрами: '3XX7'"
-        :type :tool
-        :examinable true
-        :hint "На слайде написано: 'X = номер месяца основания лаборатории'"
-      }
-      "провод" {
-        :name "Медный провод"
-        :desc "Длинный медный провод с изоляцией. Кажется, его можно использовать для починки."
-        :type :component
-        :usable-in [:promenade]
-      }
-      "схема" {
-        :name "Схема лаборатории"
-        :desc "Подробная схема всей лаборатории. Отмечены все комнаты и соединения."
-        :type :document
-        :usable-in [:promenade]
-        :info "На схеме отмечено: 'Основана: октябрь 1997'"
-      }
-      "консоль" {
-        :name "Консоль управления"
-        :desc "Центральная консоль управления лабораторией. Не работает без провода и схемы."
-        :type :device
-        :fixed false
-      }
-      "лабораторный-журнал" {
-        :name "Лабораторный журнал"
-        :desc "Старый журнал записей. На первой странице: 'Лаборатория основана в октябре 1997 года.'"
-        :type :document
-        :examinable true
-      }
-      "формулы" {
-        :name "Научные формулы"
-        :desc "Лист с математическими формулами. В углу мелко: 'Код для ежедневного доступа: 3107'"
-        :type :document
-        :examinable true
-      }
-    }
-
-        
-    ;; ИГРОВЫЕ СОБЫТИЯ (лог)
-
-    :events []
-    
-
-    ;; ВРЕМЯ ИГРЫ
-
-    :start-time (System/currentTimeMillis)
-    :game-started true
+    ;; ЗАГРУЖЕНЫ ЛИ КОМНАТЫ
+    :rooms-loaded false
   }))
 
 
 
 
-;; ОСНОВНЫЕ ФУНКЦИИ ДЛЯ РАБОТЫ С МИРОМ
+;; ЗАГРУЗКА КОМНАТ ИЗ .edn
 
+(defn load-rooms-from-edn
+  "Загрузить комнаты из resources/rooms_ru/"
+  []
+  (println "[WORLD] Загрузка комнат из EDN файлов...")
+  
+  (let [room-files ["laboratory.edn" "hallway_ru.edn" "archive.edn" 
+                    "console_room.edn" "exit.edn"]
+        loaded-rooms (atom {})]
+    
+    (doseq [room-file room-files]
+      (let [path (str "resources/rooms_ru/" room-file)]
+        (when (.exists (io/file path))
+          (try
+            (let [room-data (edn/read-string (slurp path))
+                  room-key (keyword (-> room-file
+                                        (clojure.string/replace #"_ru\.edn$" "")
+                                        (clojure.string/replace #"\.edn$" "")))]
+              (swap! loaded-rooms assoc room-key room-data)
+              (println "  ✓ Загружена:" (:name room-data)))
+            (catch Exception e
+              (println "  ✗ Ошибка загрузки" room-file ":" e))))))
+    
+    ;; Сохраняем в STM
+    (dosync
+      (alter world-state assoc :rooms @loaded-rooms)
+      (alter world-state assoc :rooms-loaded true))
+    
+    (println "  Всего комнат:" (count @loaded-rooms))
+    @loaded-rooms))
+
+;; Автоматическая загрузка при старте
+(load-rooms-from-edn)
+
+
+
+;; БАЗОВЫЕ ФУНКЦИИ
 
 (defn get-room
-  "Получить информацию о комнате (без транзакции)"
-  [room-name]
-  (get-in @world-state [:rooms room-name]))
+  "Получить комнату"
+  [room-key]
+  (get-in @world-state [:rooms room-key]))
 
 (defn get-room-name
   "Получить имя комнаты"
-  [room-name]
-  (get-in @world-state [:rooms room-name :name]))
+  [room-key]
+  (:name (get-room room-key)))
 
 (defn get-room-desc
   "Получить описание комнаты"
-  [room-name]
-  (get-in @world-state [:rooms room-name :desc]))
+  [room-key]
+  (:desc (get-room room-key)))
 
 (defn get-room-exits
   "Получить выходы из комнаты"
-  [room-name]
-  (get-in @world-state [:rooms room-name :exits]))
+  [room-key]
+  (:exits (get-room room-key)))
 
 (defn get-room-items
   "Получить предметы в комнате"
-  [room-name]
-  (get-in @world-state [:rooms room-name :items]))
+  [room-key]
+  (:items (get-room room-key)))
 
 (defn get-room-players
   "Получить игроков в комнате"
-  [room-name]
-  (get-in @world-state [:rooms room-name :players]))
+  [room-key]
+  (:players (get-room room-key)))
 
-(defn get-room-puzzle
-  "Получить головоломку в комнате"
-  [room-name]
-  (get-in @world-state [:rooms room-name :puzzle]))
-
-
-
-;; ФУНКЦИИ ОБНОВЛЕНИЯ 
-
-(defn update-room!
-  "Обновить комнату (транзакция)"
-  [room-name f & args]
-  (dosync
-    (apply alter world-state update-in [:rooms room-name] f args)))
-
-(defn add-item-to-room!
-  "Добавить предмет в комнату"
-  [room-name item]
-  (dosync
-    (alter world-state update-in [:rooms room-name :items] conj item)))
-
-(defn remove-item-from-room!
-  "Удалить предмет из комнаты"
-  [room-name item]
-  (dosync
-    (alter world-state update-in [:rooms room-name :items] disj item)))
-
-(defn move-item-between-rooms!
-  "Переместить предмет между комнатами"
-  [from-room to-room item]
-  (dosync
-    (alter world-state update-in [:rooms from-room :items] disj item)
-    (alter world-state update-in [:rooms to-room :items] conj item)))
-
-(defn add-player-to-room!
-  "Добавить игрока в комнату"
-  [room-name player-name]
-  (dosync
-    (alter world-state update-in [:rooms room-name :players] conj player-name)))
-
-(defn remove-player-from-room!
-  "Удалить игрока из комнаты"
-  [room-name player-name]
-  (dosync
-    (alter world-state update-in [:rooms room-name :players] disj player-name)))
-
-(defn move-player-between-rooms!
-  "Переместить игрока между комнатами"
-  [from-room to-room player-name]
-  (dosync
-    (alter world-state update-in [:rooms from-room :players] disj player-name)
-    (alter world-state update-in [:rooms to-room :players] conj player-name)))
-
-(defn set-puzzle-solved!
-  "Пометить головоломку как решенную"
-  [room-name]
-  (dosync
-    (alter world-state assoc-in [:rooms room-name :puzzle :solved] true)))
-
-(defn unlock-exit!
-  "Разблокировать выход из комнаты"
-  [room-name exit]
-  (dosync
-    (alter world-state update-in [:rooms room-name :locked-exits] disj exit)))
-
-(defn lock-exit!
-  "Заблокировать выход из комнаты"
-  [room-name exit]
-  (dosync
-    (alter world-state update-in [:rooms room-name :locked-exits] conj exit)))
-
-(defn exit-locked?
-  "Проверить, заблокирован ли выход"
-  [room-name exit]
-  (contains? (get-in @world-state [:rooms room-name :locked-exits]) exit))
-
-(defn get-exit-room
-  "Получить комнату по выходу (с проверкой блокировки)"
-  [from-room direction]
-  (let [exits (get-room-exits from-room)
-        target-room (get exits direction)]
-    (when (and target-room (not (exit-locked? from-room direction)))
-      target-room)))
+(defn get-all-rooms
+  "Получить все комнаты"
+  []
+  (keys (:rooms @world-state)))
 
 
 
 
-;; ФУНКЦИИ ДЛЯ РАБОТЫ С ПРЕДМЕТАМИ
-
-
-(defn get-item-info
-  "Получить информацию о предмете из реестра"
-  [item-name]
-  (get-in @world-state [:items-registry item-name]))
-
-(defn item-exists?
-  "Проверить существование предмета в реестре"
-  [item-name]
-  (contains? (get-in @world-state [:items-registry]) item-name))
-
-(defn item-in-room?
-  "Проверить, есть ли предмет в комнате"
-  [room-name item-name]
-  (contains? (get-room-items room-name) item-name))
-
-(defn item-usable-in?
-  "Проверить, можно ли использовать предмет в данной комнате"
-  [item-name room-name]
-  (let [item-info (get-item-info item-name)]
-    (when item-info
-      (contains? (set (:usable-in item-info)) room-name))))
-
-
-
-;; ФУНКЦИИ ДЛЯ ИГРОКОВ (базовые)
+;; УПРАВЛЕНИЕ ИГРОКАМИ (STM)
 
 (defn add-player!
-  "Добавить нового игрока в мир"
+  "Добавить игрока"
   [player-name]
   (dosync
-    (alter world-state assoc-in [:players player-name] 
-           {:room :start
-            :inventory #{}
-            :joined-at (System/currentTimeMillis)
-            :actions-count 0})
-    ;; Добавляем игрока в стартовую комнату
-    (alter world-state update-in [:rooms :start :players] conj player-name)))
+    (when-not (get-in @world-state [:players player-name])
+      ;; Добавляем игрока
+      (alter world-state assoc-in [:players player-name]
+             {:room :laboratory
+              :inventory #{}
+              :joined-at (System/currentTimeMillis)})
+      
+      ;; Добавляем в стартовую комнату
+      (alter world-state update-in [:rooms :laboratory :players] 
+             (fn [players] (conj (or players #{}) player-name)))
+      
+      (println "[WORLD] Игрок добавлен:" player-name)
+      player-name)))
 
 (defn remove-player!
-  "Удалить игрока из мира"
+  "Удалить игрока"
   [player-name]
   (dosync
     (let [room (get-in @world-state [:players player-name :room])]
-      ;; Удаляем из комнаты
+      ;; Убираем из комнаты
       (when room
         (alter world-state update-in [:rooms room :players] disj player-name))
-      ;; Удаляем из списка игроков
-      (alter world-state update-in [:players] dissoc player-name))))
+      
+      ;; Убираем из списка игроков
+      (alter world-state update-in [:players] dissoc player-name)
+      
+      (println "[WORLD] Игрок удален:" player-name)
+      true)))
 
 (defn get-player
-  "Получить информацию об игроке"
+  "Получить игрока"
   [player-name]
   (get-in @world-state [:players player-name]))
 
 (defn get-player-room
   "Получить комнату игрока"
   [player-name]
-  (get-in @world-state [:players player-name :room]))
+  (:room (get-player player-name)))
 
 (defn set-player-room!
   "Установить комнату игрока"
-  [player-name room-name]
+  [player-name room-key]
   (dosync
-    (alter world-state assoc-in [:players player-name :room] room-name)))
+    (let [old-room (get-player-room player-name)]
+      (when (and old-room (get-room room-key))
+        ;; Убираем из старой комнаты
+        (alter world-state update-in [:rooms old-room :players] disj player-name)
+        
+        ;; Добавляем в новую комнату
+        (alter world-state update-in [:rooms room-key :players] 
+               (fn [players] (conj (or players #{}) player-name)))
+        
+        ;; Обновляем у игрока
+        (alter world-state assoc-in [:players player-name :room] room-key)
+        
+        (println "[WORLD]" player-name "переместился из" old-room "в" room-key)
+        true))))
 
 (defn get-player-inventory
   "Получить инвентарь игрока"
   [player-name]
-  (get-in @world-state [:players player-name :inventory]))
+  (:inventory (get-player player-name)))
 
 (defn add-to-inventory!
-  "Добавить предмет в инвентарь игрока"
-  [player-name item]
+  "Добавить предмет в инвентарь"
+  [player-name item-key]
   (dosync
-    (alter world-state update-in [:players player-name :inventory] conj item)))
+    (alter world-state update-in [:players player-name :inventory] 
+           (fn [inv] (conj (or inv #{}) item-key)))
+    true))
 
 (defn remove-from-inventory!
-  "Удалить предмет из инвентаря игрока"
-  [player-name item]
+  "Удалить предмет из инвентаря"
+  [player-name item-key]
   (dosync
-    (alter world-state update-in [:players player-name :inventory] disj item)))
+    (alter world-state update-in [:players player-name :inventory] 
+           (fn [inv] (disj (or inv #{}) item-key)))
+    true))
 
 (defn player-has-item?
   "Проверить, есть ли у игрока предмет"
-  [player-name item]
-  (contains? (get-player-inventory player-name) item))
-
-
-
-
-;; ФУНКЦИИ ДЛЯ ГОЛОВОЛОМОК
-
-(defn puzzle-solved?
-  "Проверить, решена ли головоломка в комнате"
-  [room-name]
-  (get-in @world-state [:rooms room-name :puzzle :solved]))
-
-(defn get-puzzle-type
-  "Получить тип головоломки"
-  [room-name]
-  (get-in @world-state [:rooms room-name :puzzle :type]))
-
-(defn get-puzzle-hint
-  "Получить подсказку головоломки"
-  [room-name]
-  (get-in @world-state [:rooms room-name :puzzle :hint]))
-
-(defn set-global-puzzle-solved!
-  "Установить глобальную головоломку как решенную"
-  [puzzle-key]
-  (dosync
-    (alter world-state assoc-in [:puzzles puzzle-key] true)))
-
-(defn check-global-puzzle-required
-  "Проверить, выполнены ли требования для глобальной головоломки"
-  [puzzle-key]
-  (let [required (get-in @world-state [:puzzles puzzle-key :required])]
-    (every? #(get-in @world-state [:puzzles %]) required)))
-
-
-
-;; СЛУЖЕБНЫЕ ФУНКЦИИ
+  [player-name item-key]
+  (contains? (get-player-inventory player-name) item-key))
 
 (defn get-all-players
-  "Получить список всех игроков"
+  "Получить всех игроков"
   []
-  (keys (get-in @world-state [:players])))
+  (keys (:players @world-state)))
+
+
+
+;; РАБОТА С КОМНАТАМИ (STM)
+
+(defn add-item-to-room!
+  "Добавить предмет в комнату"
+  [room-key item-key]
+  (dosync
+    (alter world-state update-in [:rooms room-key :items] 
+           (fn [items] (conj (or items #{}) item-key)))
+    true))
+
+(defn remove-item-from-room!
+  "Удалить предмет из комнаты"
+  [room-key item-key]
+  (dosync
+    (alter world-state update-in [:rooms room-key :items] 
+           (fn [items] (disj (or items #{}) item-key)))
+    true))
+
+(defn item-in-room?
+  "Проверить, есть ли предмет в комнате"
+  [room-key item-key]
+  (contains? (get-room-items room-key) item-key))
+
+(defn move-item!
+  "Переместить предмет между комнатами"
+  [from-room to-room item-key]
+  (dosync
+    (remove-item-from-room! from-room item-key)
+    (add-item-to-room! to-room item-key)
+    true))
+
+(defn get-exit-room
+  "Получить комнату по выходу"
+  [from-room direction]
+  (let [exits (get-room-exits from-room)]
+    (get exits (keyword direction))))
+
+(defn get-available-exits
+  "Получить доступные выходы"
+  [room-key]
+  (let [exits (get-room-exits room-key)]
+    (into {} (map (fn [[dir target]] [dir {:room target :locked false}]) exits))))
 
 (defn get-players-in-room
   "Получить игроков в комнате (кроме указанного)"
-  [room-name exclude-player]
-  (let [all-players (get-room-players room-name)]
-    (disj all-players exclude-player)))
-
-(defn get-available-exits
-  "Получить доступные выходы из комнаты"
-  [room-name]
-  (let [exits (get-room-exits room-name)
-        locked (get-in @world-state [:rooms room-name :locked-exits])]
-    (reduce-kv (fn [acc dir target]
-                 (if (contains? locked dir)
-                   (assoc acc dir {:room target :locked true})
-                   (assoc acc dir {:room target :locked false})))
-               {} exits)))
-
-(defn reset-world!
-  "Сбросить мир к начальному состоянию (для тестов)"
-  []
-  (dosync
-    (ref-set world-state (get (var-get #'world-state) :initial-state))))
+  [room-key exclude-player]
+  (disj (get-room-players room-key) exclude-player))
 
 
 
 ;; ИНИЦИАЛИЗАЦИЯ
 
-;; Сохраняем начальное состояние для возможного сброса
-(dosync
-  (alter world-state assoc :initial-state @world-state))
+(println "[WORLD] Модуль world.clj загружен")
+(println "[WORLD] Загружено комнат:" (count (get-all-rooms)))
+(println "[WORLD] Игроков онлайн:" (count (get-all-players)))
 
-(println "[WORLD] Игровой мир инициализирован с STM системой")
-(println "[WORLD] Комнат:" (count (get-in @world-state [:rooms])))
-(println "[WORLD] Предметов в реестре:" (count (get-in @world-state [:items-registry])))
-
-;; Экспорт ключевых функций
+;; Для работы в REPL
 (comment
-  ;; Примеры использования в REPL:
+  ;; Загрузить комнаты
+  (load-rooms-from-edn)
   
-  ;; Получить состояние мира
-  @world-state
-  
-  ;; Получить комнату
-  (get-room :start)
+  ;; Проверить комнаты
+  (get-all-rooms)
+  (get-room :laboratory)
+  (get-room-name :laboratory)
   
   ;; Добавить игрока
-  (add-player! "Алексей")
+  (add-player! "Тест")
+  (get-player "Тест")
+  (get-player-room "Тест")
   
-  ;; Проверить выходы
-  (get-available-exits :hallway)
+  ;; Переместить игрока
+  (set-player-room! "Тест" :hallway_ru)
   
-  ;; Переместить предмет
-  (move-item-between-rooms! :start :hallway "ключ-карта")
+  ;; Работа с инвентарем
+  (add-to-inventory! "Тест" :keycard)
+  (get-player-inventory "Тест")
+  (player-has-item? "Тест" :keycard)
+  (remove-from-inventory! "Тест" :keycard)
   
-  ;; Проверить головоломку
-  (puzzle-solved? :hallway)
+  ;; Работа с комнатами
+  (add-item-to-room! :laboratory :wire)
+  (item-in-room? :laboratory :wire)
+  (remove-item-from-room! :laboratory :wire)
   
-  ;; STM транзакция вручную
-  (dosync
-    (alter world-state update-in [:rooms :start :items] conj "новый-предмет"))
+  ;; Получить выходы
+  (get-exit-room :laboratory :north)
+  (get-available-exits :laboratory)
+  
+  ;; Удалить игрока
+  (remove-player! "Тест")
 )
