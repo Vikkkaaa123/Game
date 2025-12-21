1c6050d2c6549fe2f0f8df46cb64be9d0e7a2636
Vikkkaaa123 <pereuznikviktoria@gmail.com>
2025-12-21 19:26:07 +0300

    Delete src/game/sync.clj

--- src/game/sync.clj
+++
@@ -1,8 +0,0 @@
-(ns game.sync
-  "Простая синхронизация")
-
-(defn sync-all-rooms []
-  (println "[SYNC] Комнаты синхронизированы (заглушка)"))
-
-(defn sync-all-players []
-  (println "[SYNC] Игроки синхронизированы (заглушка)"))
